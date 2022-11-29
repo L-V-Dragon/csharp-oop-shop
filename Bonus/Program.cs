4d7eba96922054a0bfc144e423220385eaeffc90
L-V-Dragon <viotti37@gmail.com>
2022-11-29 17:19:38 +0100

    Bonun non completo

---
+++ Bonus/Program.cs
@@ -0,0 +1,17 @@
+﻿using Bonus;
+
+Prodotto prodotto1 = new Prodotto("Supplì", "Palla di riso al sugo panata", 1.50, 22);
+Prodotto prodotto2 = new Prodotto("Assorbenti", "Panno assorbente per perdite mestruali", 3.50, 5);
+Prodotto prodotto3 = new Prodotto("Allarme anti Incendio", "Sistema di sicurezza", 50, 10);
+
+
+Console.WriteLine("Ecco dei prodotti");
+Console.WriteLine();
+
+prodotto1.StampaProdotto();
+Console.WriteLine();
+
+prodotto2.StampaProdotto();
+Console.WriteLine();
+
+prodotto3.StampaProdotto();
